626cf36fb7fe4ecedb6a5552c24d5ae1f8f368b8
iivanov99 <ivko3run@gmail.com>
2019-10-23 20:42:22 +0300

    cubicle-with-auth deleted

--- cibicle-with-auth/app-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  accessTokenSecret: '78bafe6291d042c818a71cdb2ed2d9185770af4052727988dcb2da11bcf28110bebd669e66b1881cc7a5788a',
-  authCookieName: 'auth_cookie'
-};
--- cibicle-with-auth/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  development: {
-    port: process.env.PORT || 8080,
-    dbPath: 'mongodb://localhost:27017/cubicle-part-3'
-  },
-  production: {}
-};
